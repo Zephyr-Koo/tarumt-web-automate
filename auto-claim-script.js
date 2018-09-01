9ca7435deae2eccec41e8209c7bea07edf1b9914
Zephyr Koo <30658485+Zephyr-Koo@users.noreply.github.com>
2018-09-01 20:23:29 +0800

    Create auto-claim-script.js

---
+++ auto-claim-script.js
@@ -0,0 +1,14 @@
+var group = '1DIB1';
+
+var startDate = new Date('2018-06-02');
+
+// L for Lecture, T for Tutorial, P for Practical
+var classType = 'P';
+var noOfHour  = classType == 'T' ? 1.5 : 2; // 2 or 1.5
+
+$("#id-date-picker-1").datepicker('setDate', startDate);
+$('#fgroup').val(group);
+$('select[name=funits]').prop('selectedIndex', 1); // non-default item
+$('input[name=fclass]').filter('[value="' + classType + '"]').prop("checked", true);
+$('#fhour').val(noOfHour);
+$('button[name=fsubmit]').first().click();
