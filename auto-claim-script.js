d13fef8680f3336a4ca716f30dec771881d6a440
Zephyr Koo <30658485+Zephyr-Koo@users.noreply.github.com>
2023-02-26 15:27:44 +0800

    Update auto-claim-script.js
    Update logic to easier reference

--- auto-claim-script.js
+++ auto-claim-script.js
@@ -1,14 +1,38 @@
-var group = '1DIB1';
+// Lookup
+const TeachingHours = {
+    Lecture  : 2,
+    Tutorial : 1,
+    Practical: 1
+};
 
-var startDate = new Date('2018-06-02');
+// Configuration
+let courseCode  = 'AACS3013';
+let classGroup = 'DFT1S3G18'; // DFT1S3G18, DFT1S3G19, DFT1S3G20
+let classType  = 'Tutorial'; // Lecture, Tutorial, Practical
 
-// L for Lecture, T for Tutorial, P for Practical
-var classType = 'P';
-var noOfHour  = classType == 'T' ? 1.5 : 2; // 2 or 1.5
+let claimDate = new Date('2023-02-18');
 
-$("#id-date-picker-1").datepicker('setDate', startDate);
-$('#fgroup').val(group);
-$('select[name=funits]').prop('selectedIndex', 1); // non-default item
-$('input[name=fclass]').filter('[value="' + classType + '"]').prop('checked', true);
-$('#fhour').val(noOfHour);
+let noOfHours = TeachingHours[classType];
+let classTypeValue = classType.charAt(0);
+
+// Automating claim process //
+
+// 1) Set teaching date
+$('#id-date-picker-1').datepicker('setDate', claimDate);
+
+// 2) Set class group
+$('#fgroup').val(classGroup);
+
+// 3) Set course
+$('select[name=funits]').children().filter(function() {
+    return this.text.includes(courseCode);
+}).prop('selected', true);
+
+// 4) Set class type
+$('input[name=fclass]').filter(`[value=${classTypeValue}]`).prop('checked', true);
+
+// 5) Set no. of teaching hours
+$('#fhour').val(noOfHours);
+
+// 6) Click on Add button
 $('button[name=fsubmit]').first().click();
